9b0581a020fdc57c855e5193e11e823749b69e60
zahralatif <zahralatif2107@gmail.com>
2023-09-29 14:49:07 +0400

    first commit

---
+++ NumberCheck.java
@@ -0,0 +1,33 @@
+public class NumberCheck {
+
+  public static void main(String[] args) {
+    int number1 = 21;
+    int number2 = 7;
+    int number3 = 18;
+    int number4 = 13;
+
+    if (number1 % 2 == 0) {
+      System.out.println("number1 = " + number1 + " is even");
+    } else {
+      System.out.println("number1 = " + number1 + " is odd");
+    }
+
+    if (number2 % 2 == 0) {
+      System.out.println("number2 = " + number2 + " is even");
+    } else {
+      System.out.println("number2 = " + number2 + " is odd");
+    }
+
+    if (number3 % 2 == 0) {
+      System.out.println("number3 = " + number3 + " is even");
+    } else {
+      System.out.println("number3 = " + number3 + " is odd");
+    }
+
+    if (number4 % 2 == 0) {
+      System.out.println("number4 = " + number4 + " is even");
+    } else {
+      System.out.println("number4 = " + number4 + " is odd");
+    }
+  }
+}
