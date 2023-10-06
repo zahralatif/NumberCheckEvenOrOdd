634057353f41e843709c58af5e4ba3e72eac2dd0
zahralatif <zahralatif2107@gmail.com>
2023-10-06 17:23:55 +0400

    done

--- NumberCheck.java
+++ NumberCheck.java
@@ -2,7 +2,7 @@ public class NumberCheck {
 
   public static void main(String[] args) {
     int number1 = 21;
-    int number2 = 7;
+    int number2 = -7;
     int number3 = 18;
     int number4 = 0;
 
@@ -11,31 +11,31 @@ public class NumberCheck {
     } else if (number1 % 2 == 0) {
       System.out.println("number1 = " + number1 + " is even.");
     } else {
-      System.out.println("number1 = " + number1 + " is odd");
+      System.out.println("number1 = " + number1 + " is odd.");
     }
 
     if (number2 == 0) {
       System.out.println("number2 = " + number2 + " is zero.");
     } else if (number2 % 2 == 0) {
-      System.out.println("number2 = " + number2 + " is even");
+      System.out.println("number2 = " + number2 + " is even.");
     } else {
-      System.out.println("number2 = " + number2 + " is odd");
+      System.out.println("number2 = " + number2 + " is odd.");
     }
 
     if (number3 == 0) {
       System.out.println("number3 = " + number3 + " is zero.");
     } else if (number3 % 2 == 0) {
-      System.out.println("number3 = " + number3 + " is even");
+      System.out.println("number3 = " + number3 + " is even.");
     } else {
-      System.out.println("number3 = " + number3 + " is odd");
+      System.out.println("number3 = " + number3 + " is odd.");
     }
 
     if (number4 == 0) {
       System.out.println("number4 = " + number4 + " is zero.");
     } else if (number4 % 2 == 0) {
-      System.out.println("number4 = " + number4 + " is even");
+      System.out.println("number4 = " + number4 + " is even.");
     } else {
-      System.out.println("number4 = " + number4 + " is odd");
+      System.out.println("number4 = " + number4 + " is odd.");
     }
   }
 }
